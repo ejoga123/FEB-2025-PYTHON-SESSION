fcd51260fbe2eb062e4b1dcc56c5efeec78ddc84
Ejoga Emmanuel <ejogaemmanuel62@gmail.com>
2025-03-11 23:43:07 +0100

    still part of intro to python

--- WEEK1/index.py
+++ WEEK1/index.py
@@ -1,1 +1,18 @@
 print("Hello world")
+
+number = 10
+print(number)
+
+age = 27
+marks = 90.5
+name = "John Doe"
+
+print(age)
+print(marks)
+print(name)
+
+languages = ["python", "java", "c++", "ruby", "javascript", 3, 2.4, True]
+print(languages)
+print(languages[0])
+print(languages[5])
+print(languages[-6])
